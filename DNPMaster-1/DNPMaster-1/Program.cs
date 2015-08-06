a9d13ca7b22568f806b6868db12da724890eded0
Anand Richard <kiranand@hotmail.com>
2015-08-06 08:47:34 -0400

    This will send a list of strings to the Outstation, receive them back and echo to the console

---
+++ DNPMaster-1/DNPMaster-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace DNPMaster_1
+{
+    class Program
+    {
+        public class RequestSender
+        {
+          
+            public  async Task<string> SendRequest(string data)
+            {
+                try
+                {
+                    IPAddress ipAddress = IPAddress.Loopback;  
+                
+                    TcpClient client = new TcpClient();
+                    await client.ConnectAsync(ipAddress, 50000); // Connect
+                    NetworkStream networkStream = client.GetStream();
+                    StreamWriter writer = new StreamWriter(networkStream);
+                    StreamReader reader = new StreamReader(networkStream);
+                    writer.AutoFlush = true;
+                    await writer.WriteLineAsync(data);
+                    string response = await reader.ReadLineAsync(); 
+                    client.Close();
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+
+          
+
+        };
+
+       
+
+        static void Main(string[] args)
+        {
+            Console.Title = "DNP Master Client";
+            RequestSender rS = new RequestSender();
+            string dataToSend;
+
+            for(int i=0;i<10;i++)
+            {
+                dataToSend = "Packet:: " + Convert.ToString(i);
+
+                Task<string> tsResponse = rS.SendRequest(dataToSend);
+                string reply = tsResponse.Result;
+                Console.WriteLine(reply);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+ 
---
+++ Outstation-1/Outstation-1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Outstation_1
+{
+    class Program
+    {
+        public class AsyncService
+        {
+            private async Task Process(TcpClient tcpClient)
+            {
+                string clientEndPoint =
+                tcpClient.Client.RemoteEndPoint.ToString();
+                Console.WriteLine("Received connection request from " + clientEndPoint);
+                try
+                {
+                    NetworkStream networkStream = tcpClient.GetStream();
+                    StreamReader reader = new StreamReader(networkStream);
+                    StreamWriter writer = new StreamWriter(networkStream);
+                    writer.AutoFlush = true;
+                    while (true)
+                    {
+                        string request = await reader.ReadLineAsync();
+                        if (request != null)
+                        {
+                            Console.WriteLine("Received service request: " + request);
+                            //string response = Response(request);
+                            Console.WriteLine("Computed response is: " + request + "\n");
+                            await writer.WriteLineAsync(request);
+                        }
+                        else
+                            break; // Client closed connection
+                    }
+
+                    tcpClient.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    if (tcpClient.Connected)
+                        tcpClient.Close();
+                }
+            }
+
+
+
+            public async void Run()
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, 50000);
+                listener.Start();
+                Console.Write("Array Min and Avg service is now running");
+                Console.WriteLine(" on port " + 50000);
+                Console.WriteLine("Hit <enter> to stop service\n");
+                Console.Title = "Outstation-Server";
+                while (true)
+                {
+                    try
+                    {
+                        TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                        Task t = Process(tcpClient);
+                        await t;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            private static string Response(string request)
+            {
+                string dataToSend = "Packet Received";
+                System.Threading.Thread.Sleep(1000);
+                return dataToSend;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                AsyncService service = new AsyncService();
+                service.Run();
+                Console.ReadLine();
+                 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }   
+
+    }
+}
