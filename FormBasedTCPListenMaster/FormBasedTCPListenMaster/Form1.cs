a0914e116676bbad10a37e3e0203bf389198ff8e
Anand Richard <kiranand@hotmail.com>
2015-09-01 14:17:31 -0400

    apduBuild builds message on the fly

---
+++ FormBasedTCPListenMaster/FormBasedTCPListenMaster/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace FormBasedTCPListenMaster
+{
+    public partial class Form1 : Form
+    {
+        public static TCPClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TCPClient();
+        }
+
+       private static Task<string> runClientAsync(string textToSend)
+        {
+           
+            Task<string> tsResponse = client.SendRequest(textToSend);
+           
+            return (tsResponse);
+        }
+
+        private async void btnStartMaster_Click(object sender, EventArgs e)
+        {
+            
+            string dataToSend;
+
+            for (int i = 0; i < 10; i++)
+            {
+                dataToSend = "Packet:: " + Convert.ToString(i);
+
+                string reply = await runClientAsync(dataToSend);
+                textBox1.Text += reply;
+            }
+        }
+    }
+}
---
+++ FormBasedTCPListenMaster/FormBasedTCPListenMaster/TCPListen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace FormBasedTCPListenMaster
+{
+    public class TCPClient
+    {
+        public async Task<string> SendRequest(string data)
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse("192.168.1.101");
+
+                TcpClient client = new TcpClient();
+                await client.ConnectAsync(ipAddress, 50000); // Connect
+                NetworkStream networkStream = client.GetStream();
+                StreamWriter writer = new StreamWriter(networkStream);
+                StreamReader reader = new StreamReader(networkStream);
+                writer.AutoFlush = true;
+                await writer.WriteLineAsync(data);
+                string response = await reader.ReadLineAsync();
+                client.Close();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
