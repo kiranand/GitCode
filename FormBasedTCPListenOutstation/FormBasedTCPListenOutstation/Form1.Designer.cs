c6dadf1459bb1dce78e175880665dd6930e30845
Anand Richard <kiranand@hotmail.com>
2016-02-07 10:24:46 -0500

    Added changes to talk both ways from Master to Outstation and vice-versa

--- FormBasedTCPListenOutstation/FormBasedTCPListenOutstation/Form1.Designer.cs
+++ FormBasedTCPListenOutstation/FormBasedTCPListenOutstation/Form1.Designer.cs
@@ -34,13 +34,15 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.buttonSeekDataServer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 12);
+            this.button1.Location = new System.Drawing.Point(56, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 41);
+            this.button1.Size = new System.Drawing.Size(95, 33);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start Outstation";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,18 +50,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 208);
+            this.textBox1.Location = new System.Drawing.Point(9, 169);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(277, 101);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(209, 83);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(109, 312);
+            this.label1.Location = new System.Drawing.Point(82, 254);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "CONSOLE";
             // 
@@ -67,9 +72,10 @@
             // 
             this.radioButton1.AutoCheck = false;
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(92, 81);
+            this.radioButton1.Location = new System.Drawing.Point(69, 66);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 21);
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -78,9 +84,10 @@
             // 
             this.radioButton2.AutoCheck = false;
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(92, 118);
+            this.radioButton2.Location = new System.Drawing.Point(69, 96);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(110, 21);
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
             this.radioButton2.TabIndex = 4;
             this.radioButton2.Text = "radioButton2";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -88,25 +95,39 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(95, 159);
+            this.radioButton3.Location = new System.Drawing.Point(71, 129);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(110, 21);
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
             this.radioButton3.TabIndex = 5;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "radioButton3";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
+            // buttonSeekDataServer
+            // 
+            this.buttonSeekDataServer.Location = new System.Drawing.Point(302, 11);
+            this.buttonSeekDataServer.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSeekDataServer.Name = "buttonSeekDataServer";
+            this.buttonSeekDataServer.Size = new System.Drawing.Size(95, 55);
+            this.buttonSeekDataServer.TabIndex = 6;
+            this.buttonSeekDataServer.Text = "Seek Data Server";
+            this.buttonSeekDataServer.UseVisualStyleBackColor = true;
+            this.buttonSeekDataServer.Click += new System.EventHandler(this.buttonSeekDataServer_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 338);
+            this.ClientSize = new System.Drawing.Size(440, 275);
+            this.Controls.Add(this.buttonSeekDataServer);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -122,6 +143,7 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button buttonSeekDataServer;
     }
 }
 
